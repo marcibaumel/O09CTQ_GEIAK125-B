1e080cf939e1eba3c37157d7bdf73ce3044d479d
Baumel Márton <marcibaumel@gmail.com>
2022-03-24 11:57:59 +0100

    wtf but working

--- BloodyApp/backend/src/routes/doctor.router.ts
+++ BloodyApp/backend/src/routes/doctor.router.ts
@@ -1,29 +1,37 @@
-import express from 'express'
-import {Doctor} from '../entity/Doctor'
+import express from 'express';
+import { getConnectionManager, getRepository } from 'typeorm';
+import { Doctor } from '../entity/Doctor';
 
 const router = express.Router();
-//const userRepository = MyDataSource.getRepository(Doctor)
 
 //TEST THE ROUTE
-router.get("/test", (req, res, next) => {
+router.get('/test', (req, res, next) => {
   return res.status(200).json({
-    message: "Doctor router working"
+    message: 'Doctor router working',
+  });
+});
+
+router.post('', async (req, res, next) => {
+  const repository = getRepository(Doctor);
+  const doctorEntity = repository.create(req.body);
+
+  try {
+    const entityAdded = await repository.save(doctorEntity);
+    res.status(200).json({ message: entityAdded });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.delete('/:id', (req, res, next) => {
+  console.log('Delete has started with this id:' + req.body.id);
+  //const entity = Doctor.findOneById(req.body.id);
+
+  /*
+  return res.status(201).json({
+    message: entity
   })
-})
-
-router.post("", (req, res, next)=>{
-  const doctor = new Doctor();
-
-
-
-  doctor.email = "test"
-  doctor.last_name = "test"
-  doctor.first_name = "test0"
-  doctor.password = "test00"
-
-  doctor.save();
-})
-
-
+  */
+});
 
 module.exports = router;
