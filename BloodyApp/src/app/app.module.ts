d40636bc3aa6212687551e3fb50b886732cc279c
Baumel Márton <marcibaumel@gmail.com>
2022-03-26 18:23:01 +0100

    comps

--- BloodyApp/src/app/app.module.ts
+++ BloodyApp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PlaceListComponent } from './place-list/place-list.component';
+import { NewPlaceComponent } from './new-place/new-place.component';
+import { NewDonorComponent } from './new-donor/new-donor.component';
+import { NewDonationComponent } from './new-donation/new-donation.component';
+import { DonationListComponent } from './donation-list/donation-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PlaceListComponent,
+    NewPlaceComponent,
+    NewDonorComponent,
+    NewDonationComponent,
+    DonationListComponent
   ],
   imports: [
     BrowserModule,
---
+++ BloodyApp/src/app/new-donor/new-donor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-donor',
+  templateUrl: './new-donor.component.html',
+  styleUrls: ['./new-donor.component.css']
+})
+export class NewDonorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
