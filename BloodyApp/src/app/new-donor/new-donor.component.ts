38478e079f97f6af15bd21be8814efc5476af659
Baumel Márton <marcibaumel@gmail.com>
2022-03-27 17:52:10 +0200

    donor model

--- BloodyApp/src/app/new-donor/new-donor.component.ts
+++ BloodyApp/src/app/new-donor/new-donor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-donor',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewDonorComponent implements OnInit {
 
+  form:FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(null, {
+        validators: [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(50),
+        ],
+      }),
+      sex: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      nationality: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      birth_place: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      birth_time: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      postcode: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      town: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      address: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      taj_code: new FormControl(null, {
+        validators: [],
+      }),
+    });
   }
 
 }
