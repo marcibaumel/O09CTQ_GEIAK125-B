6178c94e3c936ac13edfe266b6bbebf562f1e5eb
Baumel Márton <marcibaumel@gmail.com>
2022-03-31 15:38:03 +0200

    donation form data

--- BloodyApp/frontend/src/app/components/create-donation/create-donation.component.ts
+++ BloodyApp/frontend/src/app/components/create-donation/create-donation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-donation',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateDonationComponent implements OnInit {
 
+  form:FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      directed_taj_code: new FormControl(null, {
+        validators: [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(9),
+        ],
+      }),
+      success_donation: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      donation_date: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      directed_donation: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      directed_name: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      about: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      donor_id_fk: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      doctor_id_fk: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      donationPlace_id_fk: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+    });
   }
 
 }
