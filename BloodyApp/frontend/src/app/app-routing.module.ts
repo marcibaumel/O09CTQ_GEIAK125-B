6e0c383f5e825099e1634ed08207fba9bada250c
Baumel Márton <marcibaumel@gmail.com>
2022-04-03 17:59:15 +0200

    waiting for review

--- BloodyApp/frontend/src/app/app-routing.module.ts
+++ BloodyApp/frontend/src/app/app-routing.module.ts
@@ -7,19 +7,21 @@ import { DonationListComponent } from './components/donation-list/donation-list.
 import { LoginComponent } from './components/login/login.component';
 import { PlaceListComponent } from './components/place-list/place-list.component';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   {path:'', component: WelcomePageComponent},
   {path:'donationplaces', component: PlaceListComponent},
-  {path:'newdonationplaces', component: CreatePlaceComponent},
+  {path:'newdonationplaces', component: CreatePlaceComponent, canActivate: [AuthGuard]},
   {path:'donations', component: DonationListComponent},
-  {path:'newdonation', component: CreateDonationComponent},
-  {path:'createnewdonor', component: CreateDonorComponent},
+  {path:'newdonation', component: CreateDonationComponent, canActivate: [AuthGuard]},
+  {path:'createnewdonor', component: CreateDonorComponent, canActivate: [AuthGuard]},
   {path:'login', component:LoginComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
